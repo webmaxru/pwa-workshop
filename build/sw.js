15285ce17ca62e570b5224148fb7ea69a810ebd8
Maxim Salnikov <salnikov@gmail.com>
2017-03-15 00:19:40 +0100

    app shell added

--- build/sw.js
+++ build/sw.js
@@ -16,20 +16,50 @@ var appShellFilesToCache = [
 ]
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting())
   log('[Service Worker]: Installed')
 
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
-      log('Service Worker: Caching App Shell')
+      log('[Service Worker]: Caching App Shell')
       return cache.addAll(appShellFilesToCache)
     })
   )
 })
 
 self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
   log('[Service Worker]: Active')
+
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+
+        if (key !== cacheName) {
+          log('[Service Worker]: Removing old cache', key)
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
 })
 
 self.addEventListener('fetch', (event) => {
   log('[Service Worker]: Fetch')
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+
+      if (response) {
+        log('[Service Worker]: returning ' + event.request.url + ' from cache')
+        return response
+      } else {
+        log('[Service Worker]: returning ' + event.request.url + ' from net')
+        return fetch(event.request)
+      }
+
+      // w/o debug info: return response || fetch(e.request)
+
+    })
+  )
 })
