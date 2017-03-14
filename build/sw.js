2acaa43ef7b981d2f7701bd237824949ce008364
Maxim Salnikov <salnikov@gmail.com>
2017-03-14 23:06:23 +0100

    cleaning

--- build/sw.js
+++ build/sw.js
@@ -2,10 +2,12 @@ var log = console.log.bind(console)
 var err = console.error.bind(console)
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting())
   log('[Service Worker]: Installed')
 })
 
 self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
   log('[Service Worker]: Active')
 })
 
